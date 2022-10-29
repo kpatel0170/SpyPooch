77814e4bbeaad11c6273ab311c3a11c796556ac6
KlausMikhaelson <satyamsingh5076@gmail.com>
2022-10-30 04:03:25 +0530

    replaced codes from one file to another

--- client/src/Components/Scanningmodel.js
+++ client/src/Components/Scanningmodel.js
@@ -1,49 +1,164 @@
-import React, {useRef,useEffect, useState} from "react";
-import ReactDOM from "react-dom";
+import React, { useRef, useEffect, useState } from "react";
 
 import * as cocoSsd from "@tensorflow-models/coco-ssd";
 import "@tensorflow/tfjs";
 
 const App = () => {
-    const [record, setRecord] = useState=([]);
-    const videoElement = useRef(null)
-    const startBtn = useRef(null)
-    const stopBtn = useRef(null)
-    // initially it shouldn't start recording
-    const Record = useRef(false);
-    const modelRef = useRef(null);
-    const recordingRef = useRef(false);
-    const lastDetections = useRef([])
-    const recorderRef = useRef(null)
+  const [records, setRecords] = useState([]);
 
-    useRef(() => {
-        async function inRange() {
-            startBtn.current.setAttribute("disabled", true)
-            stopBtn.current.setAttribute("disabled", true)
-            if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-                try {
-                    const stream = await navigator.mediaDevices.getUserMedia({
-                        audio: true,
-                        video: true
-                    });
-                    window.stream = stream;
-                    videoElement.current.srcObject = stream;
-                    const model = await cocoSsd.load();
+  const videoElement = useRef(null);
+  const startButtonElement = useRef(null);
+  const stopButtonElement = useRef(null);
 
-                    modelRef.current = model;
-                    startBtn.current.removeAttribute("disabled");
-                } catch(err) {
-                    console.log(err);
-                }
-            }
+  const shouldRecordRef = useRef(false);
+  const modelRef = useRef(null);
+  const recordingRef = useRef(false);
+  const lastDetectionsRef = useRef([]);
+  const recorderRef = useRef(null);
+
+  useEffect(() => {
+    async function prepare() {
+      startButtonElement.current.setAttribute("disabled", true);
+      stopButtonElement.current.setAttribute("disabled", true);
+      if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+        try {
+          const stream = await navigator.mediaDevices.getUserMedia({
+            audio: true,
+            video: true
+          });
+          window.stream = stream;
+          videoElement.current.srcObject = stream;
+          const model = await cocoSsd.load();
+
+          modelRef.current = model;
+          startButtonElement.current.removeAttribute("disabled");
+        } catch (error) {
+          console.error(error);
         }
-        prepare();
-    }, [])
+      }
+    }
+    prepare();
+  }, []);
 
-    return(
+  async function detectFrame() {
+    if (!shouldRecordRef.current) {
+      stopRecording();
+      return;
+    }
+
+    const predictions = await modelRef.current.detect(videoElement.current);
+
+    let Animal = false;
+    for (let i = 0; i < predictions.length; i++) {
+      if (predictions[i].class === "dog" || predictions[i].class === "cat") {
+        Animal = true;
+      }
+      console.log(predictions)
+    }
+
+    if (Animal) {
+      startRecording();
+      lastDetectionsRef.current.push(true);
+    } else if (lastDetectionsRef.current.filter(Boolean).length) {
+      startRecording();
+      lastDetectionsRef.current.push(false);
+    } else {
+      stopRecording();
+    }
+
+    lastDetectionsRef.current = lastDetectionsRef.current.slice(
+      Math.max(lastDetectionsRef.current.length - 10, 0)
+    );
+
+    requestAnimationFrame(() => {
+      detectFrame();
+    });
+  }
+
+  function startRecording() {
+    if (recordingRef.current) {
+      return;
+    }
+
+    recordingRef.current = true;
+    console.log("start recording");
+
+    recorderRef.current = new MediaRecorder(window.stream);
+
+    recorderRef.current.ondataavailable = function(e) {
+      const title = new Date() + "";
+      const href = URL.createObjectURL(e.data);
+      setRecords(previousRecords => {
+        return [...previousRecords, { href, title }];
+      });
+    };
+
+    recorderRef.current.start();
+  }
+
+  function stopRecording() {
+    if (!recordingRef.current) {
+      return;
+    }
+
+    recordingRef.current = false;
+    recorderRef.current.stop();
+    console.log("stopped recording");
+    lastDetectionsRef.current = [];
+  }
+
+  return (
+    <div>
+      <div>
+        <video autoPlay playsInline muted ref={videoElement} />
+      </div>
+      <div>
         <div>
-            <video autoPlay playsInline muted ref={videoElement} />
+          <div>
+            <button
+              onClick={() => {
+                shouldRecordRef.current = true;
+                stopButtonElement.current.removeAttribute("disabled");
+                startButtonElement.current.setAttribute("disabled", true);
+                detectFrame();
+              }}
+              ref={startButtonElement}
+            >
+              Start
+            </button>
+          </div>
+          <div>
+            <button
+              onClick={() => {
+                shouldRecordRef.current = false;
+                startButtonElement.current.removeAttribute("disabled");
+                stopButtonElement.current.setAttribute("disabled", true);
+                stopRecording();
+              }}
+              ref={stopButtonElement}
+            >
+              Stop
+            </button>
+          </div>
         </div>
-    )
+        <div className="row p-3">
+          <h3>Records:</h3>
+          {!records.length
+            ? null
+            : records.map(record => {
+                return (
+                  <div className="card mt-3 w-100" key={record.title}>
+                    <div className="card-body">
+                      <h5 className="card-title">{record.title}</h5>
+                      <video controls src={record.href}></video>
+                    </div>
+                  </div>
+                );
+              })}
+        </div>
+      </div>
+    </div>
+  );
+};
 
-}
+export default App;
